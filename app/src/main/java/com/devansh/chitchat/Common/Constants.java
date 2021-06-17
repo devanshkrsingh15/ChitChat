fff226497a1536194b203e6da78b9ab987b17e60
devanshkumar15 <kumardevansh22@gmail.com>
2021-06-17 14:28:44 +0530

    FullStack Chat Application

---
+++ app/src/main/java/com/devansh/chitchat/Common/Constants.java
@@ -0,0 +1,44 @@
+package com.devansh.chitchat.Common;
+
+public class Constants {
+    public static  final  String IMAGES_FOLDER ="images";
+    public static  final  String MESSAGE_VIDEOS ="message_videos";
+    public static  final  String MESSAGE_IMAGES ="message_images";
+    public static final String REQUEST_STATUS_SENT = "sent" ;
+    public static final String REQUEST_STATUS_RECEIVED = "received" ;
+    public static final String REQUEST_STATUS_ACCEPTED ="accepted" ;
+    public static final String MESSAGE_TYPE_TEXT ="text" ;
+    public static final String MESSAGE_TYPE_IMAGE ="image" ;
+    public static final String MESSAGE_TYPE_VIDEO ="video" ;
+    public static final String NOTIFICATION_TITLE = "title";
+    public static final String NOTIFICATION_MESSAGE = "message";
+    public static final String NOTIFICATION_TO = "to";
+    public static final String NOTIFICATION_DATA = "data";
+    public static final String FIREBASE_KEY = "AAAA4Leo3vM:APA91bFqSJc4gOazAjRAHai6Qx-kUxZi49gmYq4m4qbVMRSDHbaL0l-A8a9b2-G1JEyADvpY6hdbHcXpWQ7Qi10ZcXSrPXDYvCh9a5pvMNvrvxwde2WoyWpqS9D4Wr1mxGnAeAJNJ47w";
+    public static final String SENDER_ID = "965153971955";
+    public static final String CHANNEL_ID = "chat_app_01";
+    public static final String CHANNEL_NAME ="chat_app_notifications";
+    public static final String CHANNEL_DESC ="Chat App notifications";
+    public static final String STATUS_ONLINE = "online" ;
+    public static final String STATUS_OFFLINE = "" ;
+    public static final String STATUS_TYPING = "typing..." ;
+    public static final String TYPING_STOPPED = "0" ;
+    public static final String TYPING_STARTED = "1" ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
